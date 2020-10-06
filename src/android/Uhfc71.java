55d501418a03ecb56b800b9a2e77720acaaa29a3
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2020-10-06 23:38:36 +0200

    Update Uhfc71.java

--- src/android/Uhfc71.java
+++ src/android/Uhfc71.java
@@ -26,14 +26,16 @@ public class Uhfc71 extends CordovaPlugin {
 		try {
 			 Context context = this.cordova.getActivity().getApplicationContext();
 			
+			Toast.makeText(webView.getContext(), "Costruttore", Toast.LENGTH_LONG).show();
 			InventoryUhf iu = new InventoryUhf(context, txpower);
-			
+			Toast.makeText(webView.getContext(), "Start", Toast.LENGTH_LONG).show();
 			iu.StartInventoryStream();
 			try {
 				Thread.sleep(waittime);
 			} catch (Exception e) {
 
 			}
+			Toast.makeText(webView.getContext(), "Stop", Toast.LENGTH_LONG).show();
 			iu.StopInventoryStream();
 			
 			
