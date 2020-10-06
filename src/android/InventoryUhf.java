fffff84ae4a319f7dbf3f43cafec44a09057efc5
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2020-10-06 17:05:06 +0200

    Add files via upload

---
+++ src/android/InventoryUhf.java
@@ -0,0 +1,215 @@
+package android;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.nordicid.nurapi.NurApi;
+import com.rscja.deviceapi.RFIDWithUHF;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+
+public class InventoryUhf {
+
+	public Context  mContext;
+	public Handler handler;	
+	public RFIDWithUHF mReader; 
+	public boolean loopFlag;
+
+	List<String> listaTags;
+	private HashMap<String, String> map;
+	private ArrayList<HashMap<String, String>> tagList;
+
+
+	public InventoryUhf(Context context, long txpower) {
+		super();
+		mContext = context;
+		loopFlag = false;
+		listaTags = new ArrayList<String>();
+
+		try {
+			mReader = RFIDWithUHF.getInstance();
+		} catch (Exception ex) {
+
+		}
+		try {
+			mReader.setPower((int) txpower);
+		} catch (Exception ex) {
+
+		}
+
+		tagList = new ArrayList<HashMap<String, String>>();
+
+		handler = new Handler() {
+
+			@Override
+			public void handleMessage(Message msg) {
+
+				String result = msg.obj + "";
+				String[] strs = result.split("@");
+				addEPCToList(strs[0],strs[1]);
+				//mContext.playSound(1);
+			}
+		};
+	}
+
+	public void StartInventoryStream() {
+
+		if (mReader.startInventoryTag((byte)0, (byte)0)) {
+
+			loopFlag = true;
+
+			new TagThread(80).start();
+		} else {
+			// messaggio di errore
+		}
+	}
+
+
+	/**
+	 * Stop streaming.
+	 */
+	public void StopInventoryStream()
+	{
+
+		if (loopFlag) {
+
+			loopFlag = false;
+
+			if (mReader.stopInventory()) {
+				//Stop OK
+			} else {
+				//Stop KO
+
+			}
+
+		}
+
+	}
+
+	public String GetTags() {
+		String retval = "";    	
+
+		for(int i=0; i<listaTags.size(); i++) {
+			String epcString;
+			epcString = listaTags.get(i);
+			retval = retval + epcString + ",";           
+		}   	
+
+		return retval;
+	}
+
+	private void addEPCToList(String epc,String rssi) {
+		if (!TextUtils.isEmpty(epc)) {
+			int index = checkIsExist(epc);
+
+			map = new HashMap<String, String>();
+
+			map.put("tagUii", epc);
+			map.put("tagCount", String.valueOf(1));
+			map.put("tagRssi", rssi);
+
+			if (index == -1) {
+				tagList.add(map);
+				if(!listaTags.contains(epc)) {                	
+					listaTags.add(epc);                            	
+				}
+				//LvTags.setAdapter(adapter);
+				//tv_count.setText("" + adapter.getCount());
+			} else {
+				int tagcount = Integer.parseInt(tagList.get(index).get("tagCount"), 10) + 1;
+
+				map.put("tagCount", String.valueOf(tagcount));
+
+				tagList.set(index, map);
+
+			}
+
+			//adapter.notifyDataSetChanged();
+
+		}
+	}
+
+	public int checkIsExist(String strEPC) {
+		int existFlag = -1;
+		if (isEmpty(strEPC)) {
+			return existFlag;
+		}
+
+		String tempStr = "";
+		for (int i = 0; i < tagList.size(); i++) {
+			HashMap<String, String> temp = new HashMap<String, String>();
+			temp = tagList.get(i);
+
+			tempStr = temp.get("tagUii");
+
+			if (strEPC.equals(tempStr)) {
+				existFlag = i;
+				break;
+			}
+		}
+
+		return existFlag;
+	}
+
+	public static boolean isEmpty(CharSequence cs) {
+
+		return cs == null || cs.length() == 0;
+
+	}
+
+	public static int toInt(String str, int defValue) {
+		try {
+			return Integer.parseInt(str);
+		} catch (Exception e) {
+		}
+		return defValue;
+	}
+
+
+	class TagThread extends Thread {
+
+		private int mBetween = 80;
+		HashMap<String, String> map;
+
+		public TagThread(int iBetween) {
+			mBetween = iBetween;
+		}
+
+		public void run() {
+			String strTid;
+			String strResult;
+
+			String[] res = null;
+
+			while (loopFlag) {
+
+				res = mReader.readTagFromBuffer();//.readTagFormBuffer();
+
+				if (res != null) {
+
+					strTid = res[0];
+					if (!strTid.equals("0000000000000000")&&!strTid.equals("000000000000000000000000")) {
+						strResult = "TID:" + strTid + "\n";
+					} else {
+						strResult = "";
+					}
+					Message msg = handler.obtainMessage();
+					msg.obj = strResult + "EPC:" + mReader.convertUiiToEPC(res[1]) + "@" + res[2];
+					//Log.i("msg", strResult + "EPC:" + mReader.convertUiiToEPC(res[1]) + "@" + res[2]);
+					handler.sendMessage(msg);
+				}
+				try {
+					sleep(mBetween);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+}
---
+++ src/android/Uhfc71.java
@@ -0,0 +1,79 @@
+package android;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Uhfc71 extends CordovaPlugin {
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if ("scan".equals(action)) {
+			scan(args.getString(0), args.getLong(1), args.getLong(2), callbackContext);
+			return true;
+		}
+
+		return false;
+	}
+
+	private void scan(String epc, long waittime, long txpower, CallbackContext callbackContext) {
+		try {
+			 Context context = this.cordova.getActivity().getApplicationContext();
+			
+			InventoryUhf iu = new InventoryUhf(context, txpower);
+			
+			iu.StartInventoryStream();
+			try {
+				Thread.sleep(waittime);
+			} catch (Exception e) {
+
+			}
+			iu.StopInventoryStream();
+			
+			
+			String result = "NO-TAGS";
+
+			if(epc.isEmpty()) {
+				result = iu.GetTags();
+				//result = iu.Inv();
+				
+				if(result.isEmpty()){
+					result = "NO-TAGS";
+				}
+				
+				
+								
+			}else {
+				result = iu.GetTags();
+				String tags[] = result.split(",");
+				List<String> lista = new ArrayList<String>();
+
+				for(int i=0; i<tags.length; i++) {
+					lista.add(tags[i]);
+				}
+
+				if(lista.contains(epc)) {
+					result="OK";
+				}else {
+					result="KO";
+				}
+
+			}
+
+
+			//Toast.makeText(webView.getContext(), iu.mUiResultMsg, Toast.LENGTH_LONG).show();
+			callbackContext.success(result);
+
+		} catch (Exception e) {
+			callbackContext.error(e.toString());
+		}
+
+
+	}
+}
