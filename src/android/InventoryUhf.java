630da16852f24ee41988a76eead632fff81418f2
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2020-10-08 13:07:17 +0200

    Update InventoryUhf.java

--- src/android/InventoryUhf.java
+++ src/android/InventoryUhf.java
@@ -48,7 +48,7 @@ public class InventoryUhf {
 		listaTags = new ArrayList<String>();
 		Toast.makeText(context, "0", Toast.LENGTH_LONG).show();
 		
-		/*
+		
 		try{
 			Thread.sleep(5000);
 		} catch (Exception ex) {	
@@ -79,7 +79,7 @@ public class InventoryUhf {
 		
 
 		tagList = new ArrayList<HashMap<String, String>>();
-		*/
+		
 		/*
 		handler = new Handler() {
 
